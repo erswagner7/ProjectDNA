759834f24c246bf51883c852c25c2e2c36af3573
Erich Wagner <erswagner@gmail.com>
2022-02-19 21:25:14 -0500

    Theme change

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -3,6 +3,34 @@ import React from 'react'
 
 function setColor(color){
     document.documentElement.style.setProperty('--main-bg', color)
+
+    if (color == "blue")
+    {
+        document.documentElement.style.setProperty('--second-bg', "#4682B4")
+        document.documentElement.style.setProperty('--txt-color', "#FFFFFF")
+    } else if (color == "green")
+    {
+        document.documentElement.style.setProperty('--second-bg', "#00FF00")
+        document.documentElement.style.setProperty('--txt-color', "#FFFFFF")
+    } else if (color == "red")
+    {
+        document.documentElement.style.setProperty('--second-bg', "#FF8DA7")
+        document.documentElement.style.setProperty('--txt-color', "#FFFFFF")
+    } else if (color == "black")
+    {
+        document.documentElement.style.setProperty('--second-bg', "#8A8A8A")
+        document.documentElement.style.setProperty('--txt-color', "#FFFFFF")
+    } else if (color == "white")
+    {
+        document.documentElement.style.setProperty('--second-bg', "#fafafb")
+        document.documentElement.style.setProperty('--txt-color', "#000000")
+    }
+
+
+}
+
+function setFontSize(){
+    alert("Work in Progress");
 }
 
 const Settings = () => {
@@ -27,6 +55,10 @@ const Settings = () => {
                             <button onClick={() => setColor('white')} type="primary">White</button>
 
                             <h3>Change Font Size</h3>
+                            <button onClick={() => setFontSize('')} type="primary">Small</button>
+                            <button onClick={() => setFontSize('')} type="primary">Default</button>
+                            <button onClick={() => setFontSize('')} type="primary">Large</button>
+
                         </div>
                         <div className="card__body">
                             
