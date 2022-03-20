a2847e743604b597a9fedd88a79cb541dd1b9c10
Erich Wagner <erswagner@gmail.com>
2022-03-20 13:32:13 -0400

    Fixed menu

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -27,6 +27,19 @@ const MyProfile = {
     },
   ],
 };
+const renderOrderHead = (item, index) => <th key={index}>{item}</th>;
+
+const renderOrderBody = (item, index) => (
+  <tr key={index}>
+    <td>{item.profileImage}</td>
+    <td>{item.id}</td>
+    <td>{item.username}</td>
+    <td>{item.firstname}</td>
+    <td>{item.lastname}</td>
+    <td>{item.phone}</td>
+    <td>{item.address}</td>
+  </tr>
+);
 
 const Dashboard = () => {
   const themeReducer = useSelector((state) => state.ThemeReducer.mode);
@@ -42,12 +55,12 @@ const Dashboard = () => {
               <h3>My Profile</h3>
             </div>
             <div className="card__body">
-              <Table
+              <table
                 headData={MyProfile.header}
                 renderHead={(item, index) => renderOrderHead(item, index)}
                 bodyData={MyProfile.body}
                 renderBody={(item, index) => renderOrderBody(item, index)}
-              />
+                />
             </div>
           </div>
             <div>
